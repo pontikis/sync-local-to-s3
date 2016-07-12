ad529a888900fe59262af5bafd1181e481f7bdbe
Christos Pontikis <pontikis@gmail.com>
2016-07-12 14:24:12 +0300

    Basic functionality

--- sync-local-to-s3.sh
+++ sync-local-to-s3.sh
@@ -17,6 +17,7 @@
 cmd_DATE="$(which date)"
 cmd_TEE="$(which tee)"
 S3CMD="$(which s3cmd)"
+cmd_MAIL="$(which mail)"
 
 source $1 || exit
 
@@ -29,7 +30,7 @@ echo "$ALIAS started at $dt" 2>&1 | $cmd_TEE -a $tmpBackupLog
 echo $separator 2>&1 | $cmd_TEE -a $tmpBackupLog
 
 # sync -------------------------------------------------------------------------
-$cmd_TRICKLE $S3CMD sync S3CMD_PARAMS $S3CMD_CONFIG $SOURCE $DESTINATION 2>&1 | $cmd_TEE -a $tmpBackupLog
+$cmd_TRICKLE $S3CMD sync $S3CMD_PARAMS $S3CMD_CONFIG $SOURCE $DESTINATION 2>&1 | $cmd_TEE -a $tmpBackupLog
 
 # finalize backup log ----------------------------------------------------------
 dt=`$cmd_DATE "+%Y-%m-%d %H:%M:%S"`
@@ -39,7 +40,7 @@ echo "" >> $tmpBackupLog
 cat $tmpBackupLog >> $BackupLog
 
 # send mail report -------------------------------------------------------------
-if [ $do_send_mail -eq 1 ]; then mail -s $ALIAS $mail_to  < $tmpBackupLog; fi
+if [ $do_send_mail -eq 1 ]; then $cmd_MAIL -s "$ALIAS" $mail_to  < $tmpBackupLog; fi
 
 # shutdown ---------------------------------------------------------------------
 if [ $do_shutdown -eq 1 ]; then cmd_SHUTDOWN; fi
